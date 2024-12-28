085e2d43634fa52a78bb5a30c50fe8d359251cc0
Ingrid Porfírio <123601142+PorfirioIngrid1994@users.noreply.github.com>
2024-12-28 00:14:05 -0300

    Add files via upload

---
+++ caracteristicasCasasHogwarts.js
@@ -0,0 +1,17 @@
+//Treinando Switch - Quais caracteristicas da sua casa de Hogwarts?:
+
+let casa = "Lufa-Lufa"
+
+switch (casa){
+    case "Grifinória":
+        console.log("As caracteristicas da sua casa são: Coragem, bravura e determinação.")
+        break
+    case "Sonserina":
+        console.log("As caracteristicas da sua casa são: Ambição, astúcia e liderança.")
+        break
+    case "Lufa-Lufa":
+        console.log("As caracteristicas da sua casa são: Lealdade, paciência e trabalho duro.")
+        break
+    case "Corvinal":
+        console.log("As caracteristicas da sua casa são: Inteligência, criatividade e sabedoria.")
+}
